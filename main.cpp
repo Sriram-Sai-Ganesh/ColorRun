6120ea8731f035c5c4331cd6d46f06cee6beda4a
Sriram636 <35802333+Sriram636@users.noreply.github.com>
2021-12-04 19:27:19 -0500

    Add files via upload
    added color-palette based color randomizer

--- main.cpp
+++ main.cpp
@@ -14,6 +14,18 @@ using namespace std;
 #define CHAR_HEIGHT 17
 #define CHAR_WIDTH 12
 
+// colors
+int BGCOLOR=BLACK;
+
+//button sleep duration
+#define BUTTON_PRESSED_SLEEP 300
+
+// 1 2 or 3 index for Easy, Medium or Hard
+int globalDifficultySetting=0;
+char difficultyOptions[][10] = { "Easy","Medium", "Hard"};
+const int difficultyColors[]={GREEN, ORANGE, RED};
+const int GlobalPaletteDistance=80;
+
 // menu settings
 #define menu_start_y 15
 #define menu_button_height 25
@@ -21,7 +33,7 @@ using namespace std;
 // longest menu word
 #define menu_word_length_limit 20
 
-char menuOptions[][menu_word_length_limit] = { "Play","Instructions","Difficulty","Leaderboard","Credits"};
+const char menuOptions[][menu_word_length_limit] = { "Play","Instructions","Difficulty","Leaderboard","Credits"};
 
 // return random int from lower to upper
 int getRand(int lower=0, int upper=255){
@@ -31,6 +43,77 @@ int getRand(int lower=0, int upper=255){
 // return (r,g,b) parameters as 1 int 24 bit color
 int get24BitColor(int r, int g, int b){
      return (r<<16)|(g<<8)|b;
+}
+
+// return (r,g,b) parameters as 1 int 24 bit color
+int getRand24BitColor(){
+     return (getRand()<<16)|(getRand()<<8)|getRand();
+}
+
+// get a random color within a distance d from color col
+int getNearbyColor(int color, int distance)
+{
+    int r = (color & 0xff0000) >> 16;
+    int g = (color & 0x00ff00) >> 8;
+    int b = (color & 0x0000ff);
+    int colorArr[]={r,g,b};
+    int lower, upper;
+    for(int i=0;i<3;i++)
+    {
+        if(colorArr[i]-distance<0){
+            lower=0;
+        }
+        else lower=colorArr[i]-distance;
+        if(colorArr[i]+distance>255){
+            upper=255;
+        }
+        else upper=colorArr[i]+distance;
+
+        colorArr[i]=getRand(lower, upper);
+    }
+    return get24BitColor(colorArr[0],colorArr[1],colorArr[2]);
+}
+
+// get a color from the current palettes
+int getRandPaletteColor(int distance=GlobalPaletteDistance)
+{
+    return getNearbyColor(difficultyColors[globalDifficultySetting], distance);
+}
+
+// set LCD font color to random color
+void setRandomFontColor(){
+    int color=get24BitColor(getRand(),getRand(),getRand());
+    LCD.SetFontColor(color);
+}
+
+// displays 5x5 grid of possible palettes at different distances from color.
+void displayPossiblePalette(int color, int distance, int xs, int ys)
+{
+    int lowerX=30+xs, upperX=70+xs;
+    int lowerY=30+ys, upperY=70+ys;
+    
+    for(int i=lowerX;i<upperX;i++)
+    {
+        for(int j=lowerY;j<upperY;j++)
+        {
+            LCD.SetFontColor(getNearbyColor(color,distance));
+            LCD.DrawPixel(i,j);
+            
+        }
+    }
+}
+
+// doesn't work smh. supposed to display grid of possible color palette squares
+void displayPaletteGrid(int color, int start=0, int step=10){
+    int distance=0;
+    LCD.SetFontColor(color);
+
+    for(int j=0;j<5;j++){
+        for(int i=0;i<5;i++){
+            displayPossiblePalette(color, distance,i*41,j*41);
+            distance+=10;
+        }
+    }
 }
 
 // print param text centered at (xPos, yPos)
@@ -49,13 +132,16 @@ void writeCenteredAt(char c, int xPos, int yPos)
     writeCenteredAt(text.c_str(), xPos, yPos);
 }
 
-// set LCD font color to random color
-void setRandomFontColor(){
-    int r=getRand();
-    int g=getRand();
-    int b=getRand();
-    int color=get24BitColor(r,g,b);
-    LCD.SetFontColor(color);
+// makes and displays an icon centered at (xPos, yPos)
+FEHIcon::Icon iconCenteredAt(char *text, int xPos, int yPos, int fontColor, int borderColor=BGCOLOR)
+{
+    xPos -= strlen(text) * CHAR_WIDTH/2;
+    yPos -= CHAR_HEIGHT/2;
+
+    FEHIcon::Icon button;
+    button.SetProperties(text, xPos, yPos, strlen(text)*CHAR_WIDTH, CHAR_HEIGHT, borderColor, fontColor);
+    button.Draw();
+    return button;
 }
 
 // print menu title in color and with animation
@@ -77,12 +163,14 @@ int printMenuTitleCenteredAt(int xPos, int yPos)
     int secondWordXStart=xPos+(totalWidth/2)-(letterOverlap*(CHAR_WIDTH));
     int secondWordYStart=firstWordYStart+lineSpacing;
     for(int i=0, lim=strlen(firstWord);i<lim;i++) {
-        setRandomFontColor();
+        // setRandomFontColor();
+        LCD.SetFontColor(getRandPaletteColor());
         writeCenteredAt(firstWord[i],firstWordXStart+(i*(CHAR_WIDTH+2)),firstWordYStart);
         Sleep(sleepDurationMS);
     }
     for(int i=0, lim=strlen(secondWord);i<lim;i++) {
-        setRandomFontColor();
+        // setRandomFontColor();
+        LCD.SetFontColor(getRandPaletteColor());
         writeCenteredAt(secondWord[i],secondWordXStart+(i*(CHAR_WIDTH+2)),secondWordYStart);
         Sleep(sleepDurationMS);
     }
@@ -90,8 +178,8 @@ int printMenuTitleCenteredAt(int xPos, int yPos)
 
     return 2*CHAR_HEIGHT+lineSpacing+bottomPadding;
 }
-// Prints title  with rainbow color until screen is touched, 
-// modifies x and y to return touch location
+
+// rainbow title from current palette
 void rainbowTitle(int xPos, int yPos, int *x, int *y)
 {
     char firstWord[]={"Color"};
@@ -113,17 +201,20 @@ void rainbowTitle(int xPos, int yPos, int *x, int *y)
     while(!LCD.Touch(x, y))
     {
         for(int i=0, lim=strlen(firstWord);i<lim;i++) {
-            setRandomFontColor();
+            // setRandomFontColor();
+            LCD.SetFontColor(getRandPaletteColor());
             writeCenteredAt(firstWord[i],firstWordXStart+(i*(CHAR_WIDTH+2)),firstWordYStart);
             Sleep(sleepDurationMS);
         }
         for(int i=0, lim=strlen(secondWord);i<lim;i++) {
-            setRandomFontColor();
+            // setRandomFontColor();
+            LCD.SetFontColor(getRandPaletteColor());
             writeCenteredAt(secondWord[i],secondWordXStart+(i*(CHAR_WIDTH+2)),secondWordYStart);
             Sleep(sleepDurationMS);
         }
     }
 }
+
 // print menu and return choice number based on button press
 int getMenuInput(){
 
@@ -155,11 +246,9 @@ int getMenuInput(){
     int buttonStartX=xPos-(menu_word_length_limit*CHAR_WIDTH)/2;
     int buttonEndX=xPos+(menu_word_length_limit*CHAR_WIDTH)/2;
     while(1){
-        
-        // Prints title  with rainbow color until screen is touched, 
-        // modifies x and y to return touch location
-        rainbowTitle(XLIM/2,menu_start_y+menu_button_height,&touchX, &touchY);
 
+        // while(!LCD.Touch(&touchX, &touchY));
+        rainbowTitle(XLIM/2,menu_start_y+menu_button_height,&touchX, &touchY);
         if(touchX>buttonStartX && touchX<buttonEndX)
         {
             touchY-=startY;
@@ -196,34 +285,36 @@ void play(){
 void displayInstructions(){
     
     LCD.Clear();
-    
-    char instructions[][25] = {"Tap the top","half of the screen","to jump.","Tap the bottom","half of the screen","to duck.","Jump over ____","Duck under ____","Beat the high score!"};
+    int titleColor = GRAY;
+    int instructionsColor = WHITE;
+    char instructions[][25] = {"Taaap the top","half of the screen","to jump.","Tap the bottom","half of the screen","to duck.","Jump over ____","Duck under ____","Beat the high score!"};
 
     int startX=5, startY=5;
     int lineSpacing=10;
     
-    int buttonWaitDuration=400;
     int printWaitDuration=600;
     
 
     int buttonX=XLIM-5*CHAR_WIDTH, buttonY=YLIM-2*CHAR_HEIGHT;
 
+
     int xPos=startX;
     int yPos=startY+lineSpacing*3;
 
-    LCD.WriteAt("Instructions:",startX, startY);
+
     for(int i=0, lim=sizeof(instructions)/sizeof(instructions[0]); i<lim; i++){
 
-        LCD.SetFontColor(WHITE);
+        LCD.SetFontColor(titleColor);
+        LCD.WriteAt("Instructions:",startX, startY);
+        LCD.SetFontColor(instructionsColor);
         LCD.WriteAt(instructions[i],xPos,yPos);
         Sleep(printWaitDuration);
         yPos+=CHAR_HEIGHT+lineSpacing;
 
-        if((i+1)%3==0){
+        if((i+1)%3==0 && i<lim-1){
             waitForButton("Next",buttonX, buttonY);
-            Sleep(buttonWaitDuration);
+            Sleep(BUTTON_PRESSED_SLEEP);
             LCD.Clear();
-            LCD.WriteAt("Instructions:",startX, startY);
             yPos=startY+lineSpacing*3;
         }
     }
@@ -235,9 +326,32 @@ void displayInstructions(){
 
 // display "set difficulty" message and wait for "quit" button press
 void setDifficulty(){
+    
+    
     LCD.Clear();
-    writeCenteredAt("Difficulty Here",XLIM/3,20);
-    waitForButton("Quit", XLIM-CHAR_WIDTH*5, YLIM-CHAR_HEIGHT);
+
+    FEHIcon::Icon difficultyButton = iconCenteredAt(difficultyOptions[globalDifficultySetting], XLIM/2, YLIM/2, difficultyColors[globalDifficultySetting]);
+    FEHIcon::Icon quitButton = iconCenteredAt("QUIT", XLIM-CHAR_WIDTH*5, YLIM-CHAR_HEIGHT, WHITE);
+    while (true)
+    {
+        float x, y;
+        while(!LCD.Touch(&x, &y));
+        if (difficultyButton.Pressed(x,y,0))
+        {
+            globalDifficultySetting++;
+            LCD.Clear();
+            quitButton = iconCenteredAt("QUIT", XLIM-CHAR_WIDTH*5, YLIM-CHAR_HEIGHT, WHITE);
+            globalDifficultySetting%=(sizeof difficultyOptions/sizeof difficultyOptions[0]);
+
+            difficultyButton=iconCenteredAt(difficultyOptions[globalDifficultySetting], XLIM/2, YLIM/2, difficultyColors[globalDifficultySetting]);
+            Sleep(BUTTON_PRESSED_SLEEP);
+
+        }
+        else if (quitButton.Pressed(x,y,0))
+        {
+            break;
+        }
+    }
     return;
 }
 
@@ -271,33 +385,37 @@ int main() {
         // get menu choice
         int choice=getMenuInput();
         
-        // display user selected choice
-        char text[]={"You chose \""};
-        strcat(text,menuOptions[choice]);
-        strcat(text,"\".");
-        LCD.SetFontColor(LIGHTSKYBLUE);
-        writeCenteredAt(text,XLIM/2,YLIM-CHAR_HEIGHT);
+        // // display user selected choice
+        // char text[]={"You chose \""};
+        // strcat(text,menuOptions[choice]);
+        // strcat(text,"\".");
+        // LCD.SetFontColor(LIGHTSKYBLUE);
+        // writeCenteredAt(text,XLIM/2,YLIM-CHAR_HEIGHT);
 
-        Sleep(2.0);
+        Sleep(200);
 
         // perform action based on menu choice
         switch(choice){
-            
-            case 0:
-            play();
-            break;
-            case 1:
-            displayInstructions();
-            break;
-            case 2:
-            setDifficulty();
-            break;
-            case 3:
-            displayLeaderboard();
-            break;
-            case 4:
-            displayCredits();
-            break;
+            case 0:{
+                play();
+                break;
+            }
+            case 1:{
+                displayInstructions();
+                break;
+            }
+            case 2:{
+                setDifficulty();
+                break;
+            }
+            case 3:{
+                displayLeaderboard();
+                break;
+            }
+            case 4:{
+                displayCredits();
+                break;
+            }
         }
 
     }
